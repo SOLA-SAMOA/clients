c5ccfaab33d850e66ea5a17b06fd2ff4ce9a098b
Andrew McDowell <armcdowell@hotmail.com>
2012-07-06 16:04:21 +1200

    Other Agent now displays at the top of Agents list [#22 state:fixed responsible:none]

--- beans/src/main/java/org/sola/clients/beans/party/PartySummaryListBean.java
+++ beans/src/main/java/org/sola/clients/beans/party/PartySummaryListBean.java
@@ -1,28 +1,26 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO). All rights
+ * reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this list of conditions
+ * and the following disclaimer. 2. Redistributions in binary form must reproduce the above
+ * copyright notice,this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.clients.beans.party;
@@ -37,30 +35,49 @@ import org.sola.webservices.transferobjects.EntityAction;
 import org.sola.webservices.transferobjects.casemanagement.PartySummaryTO;
 
 /**
- * Holds the list of {@link PartySummaryBean} objects. This bean is used to bind 
- * the data on the {@link ApplicationForm} for the list of agents.
+ * Holds the list of {@link PartySummaryBean} objects. This bean is used to bind the data on the {@link ApplicationForm}
+ * for the list of agents.
  */
 public class PartySummaryListBean extends AbstractBindingBean {
 
     public static final String SELECTED_PARTYSUMMARY_PROPERTY = "selectedPartySummaryBean";
+    public static final String OTHER_AGENT_NAME = "Other";
     private SolaObservableList<PartySummaryBean> partySummaryListBean;
     private PartySummaryBean selectedPartySummary;
 
-    /** Creates new instance of the object and initializes the list of {@link PartySummaryBean} objects. */
+    /**
+     * Creates new instance of the object and initializes the list of {@link PartySummaryBean}
+     * objects.
+     */
     public PartySummaryListBean() {
         partySummaryListBean = new SolaObservableList<PartySummaryBean>();
     }
-    
-    /** 
+
+    /**
      * Fills {@link ObservableList}&lt;{@link PartySummaryBean}&gt; with the list of agents.
-     * @param createDummyAgent If true, creates empty {@link PartySummaryBean} agent 
-     * to display empty option in the list.
+     *
+     * @param createDummyAgent If true, creates empty {@link PartySummaryBean} agent to display
+     * empty option in the list.
      */
     public void FillAgents(boolean createDummyAgent) {
         List<PartySummaryTO> lst = WSManager.getInstance().getCaseManagementService().getAgents();
         partySummaryListBean.clear();
-        TypeConverters.TransferObjectListToBeanList(lst, PartySummaryBean.class, (List)partySummaryListBean);
-        if(createDummyAgent){
+        TypeConverters.TransferObjectListToBeanList(lst, PartySummaryBean.class, (List) partySummaryListBean);
+
+        // Make Other the first agent in the list
+        PartySummaryBean otherAgent = null;
+        for (PartySummaryBean party : partySummaryListBean) {
+            if (OTHER_AGENT_NAME.matches(party.getFullName())) {
+                otherAgent = party;
+                break;
+            }
+        }
+        if (otherAgent != null) {
+            partySummaryListBean.remove(otherAgent);
+            partySummaryListBean.add(0, otherAgent);
+        }
+
+        if (createDummyAgent) {
             PartySummaryBean dummyAgent = new PartySummaryBean();
             dummyAgent.setName(" ");
             dummyAgent.setEntityAction(EntityAction.DISASSOCIATE);
