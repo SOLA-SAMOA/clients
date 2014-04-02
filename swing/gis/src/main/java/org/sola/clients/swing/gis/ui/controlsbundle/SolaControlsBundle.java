f08afc4545e92817a9d7c50ea981acd69a2fce00
amcdowell <armcdowell@hotmail.com>
2014-04-02 14:47:40 +1300

    Map Measure Tool [#138 responsible:none state:fixed]
    Added Measure Tool to the map. Decided against snapping the
    tool to parcel nodes as this makes it difficult to use.

--- swing/gis/src/main/java/org/sola/clients/swing/gis/ui/controlsbundle/SolaControlsBundle.java
+++ swing/gis/src/main/java/org/sola/clients/swing/gis/ui/controlsbundle/SolaControlsBundle.java
@@ -1,26 +1,30 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO). All rights
- * reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice,this list of conditions
- * and the following disclaimer. 2. Redistributions in binary form must reproduce the above
- * copyright notice,this list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution. 3. Neither the name of FAO nor the names of its
- * contributors may be used to endorse or promote products derived from this software without
- * specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
- * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 /*
@@ -43,6 +47,7 @@ import org.sola.clients.swing.gis.data.PojoDataAccess;
 import org.sola.clients.swing.gis.layer.PojoLayer;
 import org.sola.clients.swing.gis.mapaction.SolaPrint;
 import org.sola.clients.swing.gis.tool.InformationTool;
+import org.sola.clients.swing.gis.tool.MeasureTool;
 import org.sola.clients.swing.gis.ui.control.SearchPanel;
 import org.sola.common.RolesConstants;
 import org.sola.common.messaging.GisMessage;
@@ -50,9 +55,10 @@ import org.sola.webservices.search.ConfigMapLayerTO;
 import org.sola.webservices.search.MapDefinitionTO;
 
 /**
- * This is the basic abstract bundle used in Sola. It sets up the map control with common layers
- * from the layer definitions found in database. Also using the map definition the full extent and
- * srid are defined. It defines also extra resources for the SLDs.
+ * This is the basic abstract bundle used in Sola. It sets up the map control
+ * with common layers from the layer definitions found in database. Also using
+ * the map definition the full extent and srid are defined. It defines also
+ * extra resources for the SLDs.
  *
  * @author Elton Manoku
  */
@@ -82,8 +88,8 @@ public abstract class SolaControlsBundle extends ControlsBundle {
     /**
      * Sets up the bundle.
      *
-     * @param pojoDataAccess The data access library used to communicate with the server from where
-     * the map definitions are retrieved.
+     * @param pojoDataAccess The data access library used to communicate with
+     * the server from where the map definitions are retrieved.
      *
      */
     public void Setup(PojoDataAccess pojoDataAccess) {
@@ -109,6 +115,13 @@ public abstract class SolaControlsBundle extends ControlsBundle {
             if (SecurityBean.isInRole(RolesConstants.GIS_EXPORT_MAP)) {
                 this.getMap().addMapAction(new KMLExportAction(this.getMap()), this.getToolbar(), true);
             }
+
+            // Ticket #138 Add new Measure tool
+            if (SecurityBean.isInRole(RolesConstants.GIS_MEASURE_TOOL)) {
+                MeasureTool measureTool = new MeasureTool();
+                this.getMap().addTool(measureTool, this.getToolbar(), true);
+            }
+            
             this.getMap().setFullExtent(
                     mapDefinition.getEast(),
                     mapDefinition.getWest(),
@@ -181,8 +194,9 @@ public abstract class SolaControlsBundle extends ControlsBundle {
     /**
      * Refreshes the map
      *
-     * @param force True = If a layer can be marked to be refreshed always, it will be refreshed
-     * even if the extent of the map is not changed. It is used when overridden by sub-classes.
+     * @param force True = If a layer can be marked to be refreshed always, it
+     * will be refreshed even if the extent of the map is not changed. It is
+     * used when overridden by sub-classes.
      */
     public void refresh(boolean force) {
         this.getMap().refresh();
